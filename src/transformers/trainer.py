36318009f13896b2349d5543f8706ceba76e34d4
shunki <bono0504@icloud.com>
2023-04-10 19:52:22 +0900

    style: formatting

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -1422,8 +1422,9 @@ class Trainer:
         elif self.fsdp is not None:
             if not self.args.fsdp_config["xla"]:
                 # PyTorch FSDP!
-                from torch.distributed.fsdp.fully_sharded_data_parallel import CPUOffload, MixedPrecision
+                from torch.distributed.fsdp.fully_sharded_data_parallel import CPUOffload
                 from torch.distributed.fsdp.fully_sharded_data_parallel import FullyShardedDataParallel as FSDP
+                from torch.distributed.fsdp.fully_sharded_data_parallel import MixedPrecision
                 from torch.distributed.fsdp.wrap import size_based_auto_wrap_policy, transformer_auto_wrap_policy
 
                 if FSDPOption.OFFLOAD in self.args.fsdp:
